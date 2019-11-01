27d8d9140c0d4e69b4fc42d0905ad62cded3fa58
John Choi <officialjonguk@gmail.com>
2019-11-01 11:09:45 -0700

    Updated Register screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {
-    Image,
     View,
     Alert
 } from 'react-native';
 
-import { Container, Content, Item, Label, Input, Button, Text } from 'native-base';
+import { Container, Content, Item, Label, Input, Button, Text, H1 } from 'native-base';
 
 const HomeScreen = ({ navigation }) => {
-    const [username, onChangeUsername] = React.useState('');
+    const [email, onChangeEmail] = React.useState('');
     const [password, onChangePassword] = React.useState('');
 
     const onPressSignIn = () => {
-        Alert.alert(`Username: ${username}\nPassword: ${password}`);
+        Alert.alert(`Email: ${email}\nPassword: ${password}`);
     }
 
     const onPressSignUp = () => {
@@ -26,17 +25,18 @@ const HomeScreen = ({ navigation }) => {
     return (
         <Container>
             <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
-                <View style={{ alignItems: 'center' }}>
-                    <Image source={require('../assets/images/billy_bronco.png')} />
+                <View style={{ padding: 10, alignItems: 'center' }}>
+                    <H1>Account Credentials</H1>
                 </View>
 
                 <View style={{ padding: 10 }}>
                     <Item inlineLabel>
-                        <Label>Username</Label>
-                        <Input onChangeText={text => onChangeUsername(text)} value={username} />
+                        <Label>Email:</Label>
+                        <Input onChangeText={text => onChangeEmail(text)} value={email} />
                     </Item>
+
                     <Item inlineLabel last>
-                        <Label>Password</Label>
+                        <Label>Password:</Label>
                         <Input onChangeText={text => onChangePassword(text)} value={password} />
                     </Item>
                 </View>
@@ -45,6 +45,7 @@ const HomeScreen = ({ navigation }) => {
                     <View style={{ flex: 1, paddingRight: 5 }}>
                         <Button primary style={{ justifyContent: 'center' }} onPress={onPressSignIn}><Text>Sign In</Text></Button>
                     </View>
+
                     <View style={{ flex: 1, paddingLeft: 5 }}>
                         <Button info style={{ justifyContent: 'center' }} onPress={onPressSignUp}><Text>Sign Up</Text></Button>
                     </View>
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,16 +1,92 @@
 import React from 'react';
 import {
-    View,
+    View
 } from 'react-native';
 
-import { Container, Content, Text } from 'native-base';
+import { Container, Button, Content, Text, H1, H3, Item, Input, Label, List, ListItem, Picker, Icon } from 'native-base';
+import Divider from 'react-native-divider';
+
 
 const RegisterScreen = () => {
+    const [businessType, onChangeBusinessType] = React.useState('');
+    const [email, onChangeEmail] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
+
     return (
         <Container>
-            <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
-                <View style={{ alignItems: 'center' }}>
-                    <Text>This is a register page.</Text>
+            <Content>
+                <View style={{ alignItems: 'center', paddingTop: 30 }}>
+                    <H1>Welcome to the POS!</H1>
+                    <Text style={{ fontSize: 12 }}>Please describe your business.</Text>
+                </View>
+
+                <View style={{ padding: 10, paddingTop: 30 }}>
+                    <Divider><H3 style={{ fontWeight: 'bold' }}>Credentials</H3></Divider>
+
+                    <Item inlineLabel>
+                        <Label>Email:</Label>
+                        <Input onChangeText={text => onChangeEmail(text)} value={email} />
+                    </Item>
+
+                    <Item inlineLabel last>
+                        <Label>Password:</Label>
+                        <Input onChangeText={text => onChangePassword(text)} value={password} />
+                    </Item>
+                </View>
+
+                <View style={{ padding: 10, paddingTop: 20 }}>
+                    <Divider><H3 style={{ fontWeight: 'bold' }}>About Your Business</H3></Divider>
+
+                    <Item inlineLabel>
+                        <Label>Business Name:</Label>
+                        <Input />
+                    </Item>
+
+                    <Item inlineLabel>
+                        <Label>Business Address:</Label>
+                        <Input />
+                    </Item>
+
+                    <Item inlineLabel picker>
+                        <Label>Type of Business:</Label>
+                        <Picker
+                            mode='dropdown'
+                            iosIcon={<Icon name='arrow-down' />}
+                            placeholder='Select Type'
+                            placeholderStyle={{ color: '#bfc6ea' }}
+                            placeholderIconColor='#007aff'
+                            onValueChange={val => onChangeBusinessType(val)}
+                            selectedValue={businessType}
+                        >
+                            <Picker.Item label='Restaurant' value='key0' />
+                            <Picker.Item label='Salon' value='key1' />
+                            <Picker.Item label='Repair Shop' value='key2' />
+                            <Picker.Item label='Convenience Store' value='key3' />
+                        </Picker>
+                    </Item>
+                </View>
+
+                <View style={{ padding: 10, paddingTop: 20 }}>
+                    <Divider><H3 style={{ fontWeight: 'bold' }}>Employees</H3></Divider>
+                    <List>
+                        <ListItem>
+                            <Text>Simon Mignolet</Text>
+                        </ListItem>
+                        <ListItem>
+                            <Text>Nathaniel Clyne</Text>
+                        </ListItem>
+                        <ListItem>
+                            <Text>Dejan Lovren</Text>
+                        </ListItem>
+                    </List>
+                </View>
+
+                <View style={{ flexDirection: 'row', padding: 10, justifyContent: 'flex-end' }}>
+                    <Button info><Text>Add New Employee</Text></Button>
+                </View>
+
+                <View style={{ padding: 10 }}>
+                    <Button primary style={{ justifyContent: 'center' }}><Text>Continue</Text></Button>
                 </View>
             </Content>
         </Container >
