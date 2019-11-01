b6c2ac601768e1ab482b70be036193d628f89878
John Choi <officialjonguk@gmail.com>
2019-11-01 04:39:26 -0700

    Added NativeBase and Sign In components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,212 +1,72 @@
-import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+    Image,
+    View,
+    Alert
 } from 'react-native';
-import { Button } from 'react-native-elements';
-import { MonoText } from '../components/StyledText';
+
+import { Container, Header, Title, Content, Item, Label, Input, Button, Left, Right, Body, Text } from 'native-base';
 
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/billy_bronco.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+    const [username, onChangeUsername] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+    const onPressSignIn = () => {
+        Alert.alert(`Username: ${username}\nPassword: ${password}`);
+    }
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
+    const onPressSignUp = () => {
+        Alert.alert('You have pressed the sign up button.');
+    }
 
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
+    const onPressForgotPassword = () => {
+        Alert.alert('You have pressed the forgot password button.');
+    }
 
-          <Text style={styles.getStartedText}>
-            Testing the changes.
-          </Text>
-        </View>
+    return (
+        <Container>
+            <Header>
+                <Left />
+                <Body>
+                    <Title>Sign In</Title>
+                </Body>
+                <Right />
+            </Header>
 
-        <View style={styles.chargeContainer}>
-          <Button
-            buttonStyle={styles.button}
-            title="Charge"
-            onPress={() => {}}
-          />
-        </View>
+            <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <Image source={require('../assets/images/billy_bronco.png')} />
+                </View>
 
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+                <View style={{ padding: 10 }}>
+                    <Item inlineLabel>
+                        <Label>Username</Label>
+                        <Input onChangeText={text => onChangeUsername(text)} value={username} />
+                    </Item>
+                    <Item inlineLabel last>
+                        <Label>Password</Label>
+                        <Input onChangeText={text => onChangePassword(text)} value={password} />
+                    </Item>
+                </View>
 
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
+                <View style={{ flexDirection: 'row', padding: 10 }}>
+                    <View style={{ flex: 1, paddingRight: 5 }}>
+                        <Button primary style={{ justifyContent: 'center' }} onPress={onPressSignIn}><Text>Sign In</Text></Button>
+                    </View>
+                    <View style={{ flex: 1, paddingLeft: 5 }}>
+                        <Button info style={{ justifyContent: 'center' }} onPress={onPressSignUp}><Text>Sign Up</Text></Button>
+                    </View>
+                </View>
 
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
-  );
+                <View style={{ padding: 10, paddingTop: -5 }}>
+                    <Button light style={{ justifyContent: 'center' }} onPress={onPressForgotPassword}><Text>Forgot Password</Text></Button>
+                </View>
+            </Content>
+        </Container >
+    );
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+    header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
-const styles = StyleSheet.create({
-  button: {
-       backgroundColor: '#e60f00',
-       borderColor: '#fff',
-       marginHorizontal: 30,
-       marginVertical: 50
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
