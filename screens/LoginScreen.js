556e12b31192c66b300287f1e29568e2cc7426f8
John Choi <officialjonguk@gmail.com>
2019-11-16 21:19:56 -0800

    Change HomeScreen name to LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { View, Alert } from 'react-native';
 import { Container, Content, Item, Label, Input, Button, Text, H1 } from 'native-base';
 
-const HomeScreen = ({ navigation }) => {
-
+const LoginScreen = ({ navigation }) => {
     const [email, onChangeEmail] = React.useState('');
     const [password, onChangePassword] = React.useState('');
 
@@ -25,57 +24,57 @@ const HomeScreen = ({ navigation }) => {
 
     return (
         <Container>
-            <Content contentContainerStyle = {{ justifyContent: 'center', flex: 1 }}>
-                <View style = {{ padding: 10, alignItems: 'center'}}>
-                    <H1 style = {{ fontWeight: 'bold' }}>Account Credentials</H1>
+            <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
+                <View style={{ padding: 10, alignItems: 'center' }}>
+                    <H1 style={{ fontWeight: 'bold' }}>Account Credentials</H1>
                 </View>
 
-                <View style = {{ padding: 10 }}>
+                <View style={{ padding: 10 }}>
                     <Item inlineLabel>
                         <Label>Email:</Label>
                         <Input
-                            keyboardAppearance = 'dark'
-                            onChangeText = {text => onChangeEmail(text)}
-                            value = {email}
+                            keyboardAppearance='dark'
+                            onChangeText={text => onChangeEmail(text)}
+                            value={email}
                         />
                     </Item>
 
                     <Item inlineLabel last>
                         <Label>Password:</Label>
                         <Input
-                            keyboardAppearance = 'dark'
-                            onChangeText = {text => onChangePassword(text)}
-                            value = {password}
+                            keyboardAppearance='dark'
+                            onChangeText={text => onChangePassword(text)}
+                            value={password}
                         />
                     </Item>
                 </View>
 
-                <View style = {{ flexDirection: 'row', padding: 10 }}>
-                    <View style = {{ flex: 1, paddingRight: 5 }}>
-                        <Button primary style = {{ justifyContent: 'center', backgroundColor: '#0a8508' }}
-                            onPress = {onPressSignIn}>
-                            <Text style = {{ fontWeight: 'bold' }}>Sign In</Text>
+                <View style={{ flexDirection: 'row', padding: 10 }}>
+                    <View style={{ flex: 1, paddingRight: 5 }}>
+                        <Button primary style={{ justifyContent: 'center', backgroundColor: '#0a8508' }}
+                            onPress={onPressSignIn}>
+                            <Text style={{ fontWeight: 'bold' }}>Sign In</Text>
                         </Button>
                     </View>
 
-                    <View style = {{ flex: 1, paddingLeft: 5 }}>
-                        <Button info style = {{ justifyContent: 'center', backgroundColor: '#4c614c' }}
-                            onPress = {onPressSignUp}>
-                            <Text style = {{ fontWeight: 'bold' }}>Sign Up</Text>
+                    <View style={{ flex: 1, paddingLeft: 5 }}>
+                        <Button info style={{ justifyContent: 'center', backgroundColor: '#4c614c' }}
+                            onPress={onPressSignUp}>
+                            <Text style={{ fontWeight: 'bold' }}>Sign Up</Text>
                         </Button>
                     </View>
                 </View>
 
-                <View style = {{ padding: 10, paddingTop: -5 }}>
-                    <Button light style = {{ justifyContent: 'center' }}
-                        onPress = {onPressForgotPassword}>
+                <View style={{ padding: 10, paddingTop: -5 }}>
+                    <Button light style={{ justifyContent: 'center' }}
+                        onPress={onPressForgotPassword}>
                         <Text>Forgot Password</Text>
                     </Button>
                 </View>
 
-                <View style = {{ padding: 10, paddingTop: -10 }}>
-                    <Button light style = {{ justifyContent: 'center' }}
-                        onPress = {onPressCharge}>
+                <View style={{ padding: 10, paddingTop: -10 }}>
+                    <Button light style={{ justifyContent: 'center' }}
+                        onPress={onPressCharge}>
                         <Text>[PH]Charge Page</Text>
                     </Button>
                 </View>
@@ -84,8 +83,8 @@ const HomeScreen = ({ navigation }) => {
     );
 }
 
-HomeScreen.navigationOptions = {
+LoginScreen.navigationOptions = {
     title: 'Sign In'
 };
 
-export default HomeScreen
+export default LoginScreen
